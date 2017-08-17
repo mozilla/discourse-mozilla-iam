7ac207c3f34b1e556578df7d7900292dd90f89a5
Leo McArdle <leomcardle@gmail.com>
2017-08-17 13:48:58 +0100

    fix bug with associated group being nil

---
+++ spec/models/group_mapping_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../iam_helper'
+
+describe MozillaIAM::GroupMapping do
+  it 'should be destroyed when associated group is destroyed' do
+    group = Fabricate(:group)
+    mapping = MozillaIAM::GroupMapping.new(iam_group_name: 'iam_group',
+                                 authoritative: false,
+                                 group: group)
+    mapping.save!
+    mapping.reload
+
+    expect(mapping).to be
+    expect(mapping.group).to be
+
+    mapping.group.destroy!
+
+    expect(MozillaIAM::GroupMapping.first).to be_nil
+  end
+end
