24788c53d04604d6fca5aed02eb4033111374879
Leo McArdle <leomcardle@gmail.com>
2017-09-07 14:53:57 +0100

    FEATURE: add iam details to admin user page

--- plugin.rb
+++ plugin.rb
@@ -22,3 +22,15 @@ auth_provider(title: 'Mozilla',
               message: 'Log In / Sign Up',
               authenticator: MozillaIAM::Authenticator.new('auth0', trusted: true),
               full_screen_login: true)
+
+after_initialize do
+
+  add_to_serializer(:AdminDetailedUser, :mozilla_iam, false) do
+    object.custom_fields.select do |k, v|
+      k.start_with?('mozilla_iam')
+    end.map do |k, v|
+      [k.sub('mozilla_iam_', ''), v]
+    end.to_h
+  end
+
+end
