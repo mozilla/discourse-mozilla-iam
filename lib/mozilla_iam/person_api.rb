5c2b14270c40e7e9cfe9ead3b9459122dc99f01c
Leo McArdle <leomcardle@gmail.com>
2018-02-28 20:56:14 +0000

    update person api url

--- lib/mozilla_iam/person_api.rb
+++ lib/mozilla_iam/person_api.rb
@@ -3,7 +3,7 @@ module MozillaIAM
 
     def initialize(config={})
       config = {
-        url: SiteSetting.mozilla_iam_person_api_url,
+        url: SiteSetting.mozilla_iam_person_api_url + "/v1",
         aud: SiteSetting.mozilla_iam_person_api_aud
       }.merge(config)
       super(config)
