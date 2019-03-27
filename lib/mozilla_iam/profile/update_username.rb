743e186cf11a716a6f825caae43fd3b28c5a8094
Leo McArdle <lmcardle@mozilla.com>
2019-03-27 18:32:25 +0000

    FEATURE: Implement username refresh method
    Updates username from Person API v2 for users with dinopark_enabled?
    set to true, after sanitization to ensure it meets Discourse's
    requirements.
    
    https://github.com/mozilla/discourse/issues/183

---
+++ lib/mozilla_iam/profile/update_username.rb
@@ -0,0 +1,25 @@
+module MozillaIAM
+  Profile.class_eval do
+    during_refresh :update_username
+
+    def update_username
+      return unless dinopark_enabled?
+
+      username = attr(:username).dup
+      return if username.blank?
+      return if username == @user.username
+      return if User.reserved_username? username
+
+      if @user.username.downcase != username.downcase
+        username = UserNameSuggester.find_available_username_based_on(username)
+      end
+
+      unless @user.change_username(username)
+        Rails.logger.error <<~EOF
+          Mozilla IAM: Error updating username for user #{@user.id}
+          current username: #{@user.username}, new username: #{username}, dinopark username: #{attr(:username)}
+        EOF
+      end
+    end
+  end
+end
