mozilla/discourse-mozilla-iam

commit 2c9cc360105537c36e92ebcd2b227f874565f5e3

FEATURE: Prevent editing of synced fields from DinoPark
https://github.com/mozilla/discourse/issues/187

lib/mozilla_iam/users_controller_extensions.rb
@@ -10,5 +10,35 @@ module MozillaIAM
       super
     end
 
+    def user_params
+      result = super
+      if Profile.for(fetch_user_from_params)&.dinopark_enabled?
+        [
+          :name,
+          :title,
+          :bio_raw,
+          :location,
+          :website
+        ].each { |x| result.delete(x) }
+      end
+      result
+    end
+
+    def username
+      if Profile.for(fetch_user_from_params)&.dinopark_enabled?
+        render_json_error(I18n.t("dinopark.update_username"))
+      else
+        super
+      end
+    end
+
+    def pick_avatar
+      if Profile.for(fetch_user_from_params)&.dinopark_enabled?
+        render_json_error(I18n.t("dinopark.update_avatar"))
+      else
+        super
+      end
+    end
+
   end
 end

plugin.rb
@@ -1,6 +1,6 @@
 # name: mozilla-iam
 # about: A plugin to integrate Discourse with Mozilla's Identity and Access Management (IAM) system
-# version: 1.2.0-alpha.16
+# version: 1.2.0-alpha.17
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-mozilla-iam
 

spec/requests/users_controller_spec.rb
@@ -1,0 +1,121 @@
+require_relative "../iam_helper"
+
+describe UsersController do
+  let!(:user) { sign_in(Fabricate(:user, admin: true)) }
+  let(:profile) { MozillaIAM::Profile.new(user, "uid") }
+
+  describe '#username' do
+    let(:new_username) { user.username + "_1" }
+
+    context "without dinopark_enabled" do
+      it "allows update to username" do
+        put "/u/#{user.username}/preferences/username.json", params: { new_username: new_username }
+
+        expect(response.status).to eq(200)
+        expect(user.reload.username).to eq(new_username)
+      end
+    end
+
+    context "with dinopark_enabled" do
+      let(:old_username) { user.username }
+      before do
+        profile.dinopark_enabled = true
+      end
+
+      it "doesn't allow update to username" do
+        put "/u/#{user.username}/preferences/username.json", params: { new_username: new_username }
+
+        expect(response.status).to eq(422)
+        expect(user.reload.username).to eq(old_username)
+      end
+    end
+
+  end
+
+  describe '#pick_avatar' do
+    let(:upload) { Fabricate(:upload, user: user) }
+
+    context "without dinopark_enabled" do
+      it "allows update to avatar" do
+        put "/u/#{user.username}/preferences/avatar/pick.json", params: {
+          upload_id: upload.id, type: "custom"
+        }
+
+        expect(response.status).to eq(200)
+        expect(user.reload.uploaded_avatar_id).to eq(upload.id)
+      end
+    end
+
+    context "with dinopark_enabled" do
+      before do
+        profile.dinopark_enabled = true
+      end
+
+      it "doesn't allow update to avatar" do
+        put "/u/#{user.username}/preferences/avatar/pick.json", params: {
+          upload_id: upload.id, type: "custom"
+        }
+
+        expect(response.status).to eq(422)
+        expect(user.reload.uploaded_avatar_id).to_not eq(upload.id)
+      end
+    end
+  end
+
+  describe '#update' do
+    let!(:old_email_level) { user.user_option.email_level }
+    let(:update_params) do
+      {
+        name: "new name",
+        title: "new title",
+        bio_raw: "new bio",
+        location: "new location",
+        website: "http://new.website/",
+        email_level: UserOption.email_level_types[:always]
+      }
+    end
+
+    context "without dinopark_enabled" do
+      it "allows update to synced fields" do
+        put "/u/#{user.username}.json", params: update_params
+
+        user.reload
+
+        expect(user.name).to eq("new name")
+        expect(user.title).to eq("new title")
+        expect(user.user_profile.bio_raw).to eq("new bio")
+        expect(user.user_profile.location).to eq("new location")
+        expect(user.user_profile.website).to eq("http://new.website/")
+
+        expect(user.user_option.email_level).to_not eq(old_email_level)
+        expect(user.user_option.email_level).to eq(UserOption.email_level_types[:always])
+      end
+    end
+
+    context "with dinopark_enabled" do
+      let!(:old_name) { user.name }
+      let!(:old_title) { user.title }
+      let!(:old_bio) { user.user_profile.bio_raw }
+      let!(:old_location) { user.user_profile.location }
+      let!(:old_website) { user.user_profile.website }
+      before do
+        profile.dinopark_enabled = true
+      end
+
+      it "doesn't allow update to synced fields" do
+        put "/u/#{user.username}.json", params: update_params
+
+        user.reload
+
+        expect(user.name).to eq(old_name)
+        expect(user.title).to eq(old_title)
+        expect(user.user_profile.bio_raw).to eq(old_bio)
+        expect(user.user_profile.location).to eq(old_location)
+        expect(user.user_profile.website).to eq(old_website)
+
+        expect(user.user_option.email_level).to_not eq(old_email_level)
+        expect(user.user_option.email_level).to eq(UserOption.email_level_types[:always])
+      end
+    end
+  end
+end

config/locales/server.en.yml
@@ -9,6 +9,9 @@ en:
     mozilla_iam_person_v2_api_aud: 'Audience for the Mozilla IAM Person API v2'
     dinopark_url: 'URL to DinoPark'
     mozilla_iam_notification_aud: 'Audience of Mozilla IAM Notifications'
+  dinopark:
+    update_username: "Sorry, you can't edit your username on Discourse. Please edit it on the Mozilla People Directory instead."
+    update_avatar: "Sorry, you can't edit your avatar on Discourse. Please edit it on the Mozilla People Directory instead."
   mozilla_iam:
     need_help: "Need help?"
     authenticator:

commit b447fdc0fd63a9591d195d737793d262433964b8

FIX: sign-up was broken by previous commit
https://github.com/mozilla/discourse-mozilla-iam/commit/2c9cc360105537c36e92ebcd2b227f874565f5e3

Also adds specs for https://github.com/mozilla/discourse-mozilla-iam/commit/c9e43eadf670e8ef6a157d2adb726da4983d5af9
https://github.com/mozilla/discourse/issues/186

lib/mozilla_iam/users_controller_extensions.rb
@@ -3,7 +3,8 @@ module MozillaIAM
 
     def create
       params.permit(:dinopark_enabled)
-      if dinopark_enabled = params[:dinopark_enabled]
+      if dinopark_enabled = (params[:dinopark_enabled] == "true")
+        session[:authentication] = {} unless session[:authentication]
         session[:authentication][:dinopark_enabled] = dinopark_enabled
         params[:username] = UserNameSuggester.find_available_username_based_on(params[:username])
       end
@@ -12,14 +13,17 @@ module MozillaIAM
 
     def user_params
       result = super
-      if Profile.for(fetch_user_from_params)&.dinopark_enabled?
-        [
-          :name,
-          :title,
-          :bio_raw,
-          :location,
-          :website
-        ].each { |x| result.delete(x) }
+      begin
+        if Profile.for(fetch_user_from_params)&.dinopark_enabled?
+          [
+            :name,
+            :title,
+            :bio_raw,
+            :location,
+            :website
+          ].each { |x| result.delete(x) }
+        end
+      rescue Discourse::NotFound
       end
       result
     end

plugin.rb
@@ -1,6 +1,6 @@
 # name: mozilla-iam
 # about: A plugin to integrate Discourse with Mozilla's Identity and Access Management (IAM) system
-# version: 1.2.0-alpha.17
+# version: 1.2.0-alpha.18
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-mozilla-iam
 

spec/components/auth/result_spec.rb
@@ -1,0 +1,43 @@
+require_relative "../../iam_helper"
+
+describe Auth::Result do
+  describe "#to_client_hash" do
+    let(:result) do
+      result = described_class.new
+      result.email = "janebloggs@example.com"
+      result.username = "janebloggs"
+      result.user_id = "uid"
+      result
+    end
+    let(:hash) { result.to_client_hash }
+
+    before do
+      MozillaIAM::API::PersonV2.any_instance.expects(:get).returns({})
+    end
+
+    context "without person v2 profile" do
+      before do
+        MozillaIAM::API::PersonV2::Profile.any_instance.expects(:blank?).returns(true)
+      end
+
+      it "doesn't include dinopark_profile attribute" do
+        expect(hash[:username]).to eq "janebloggs"
+        expect(hash[:dinopark_profile]).to be_nil
+      end
+    end
+
+    context "with person v2 profile" do
+      before do
+        MozillaIAM::API::PersonV2::Profile.any_instance.expects(:blank?).returns(false)
+        MozillaIAM::API::PersonV2::Profile.any_instance.expects(:to_hash).returns({
+          username: "janebloggs"
+        })
+      end
+
+      it "includes dinopark_profile attribute" do
+        expect(hash[:username]).to eq "janebloggs"
+        expect(hash[:dinopark_profile][:username]).to eq "janebloggs"
+      end
+    end
+  end
+end

spec/components/mozilla_iam/api/person_v2_spec.rb
@@ -177,5 +177,41 @@ describe MozillaIAM::API::PersonV2 do
         end
       end
     end
+
+    describe "#blank?" do
+      it "passes through #blank? on @raw" do
+        raw = {}
+        raw.expects(:blank?).returns(:mocked_response)
+        profile = described_class.new(raw)
+        expect(profile.blank?).to eq(:mocked_response)
+      end
+    end
+
+    describe "#to_hash" do
+      it "creates hash of all attributes" do
+        profile = profile_with(
+          primary_username: "username_value",
+          pronouns: "pronouns_value",
+          first_name: "first_name_value",
+          last_name: "last_name_value",
+          fun_title: "fun_title_value",
+          description: "description_value",
+          location: "location_value",
+          picture: "picture_value"
+        )
+
+        hash = profile.to_hash
+        expect(hash["raw"]).to be_nil
+        expect(hash.length).to eq 8
+        expect(hash["username"]).to eq("username_value")
+        expect(hash["pronouns"]).to eq("pronouns_value")
+        expect(hash["full_name"]).to eq("first_name_value last_name_value")
+        expect(hash["fun_title"]).to eq("fun_title_value")
+        expect(hash["description"]).to eq("description_value")
+        expect(hash["location"]).to eq("location_value")
+        expect(hash["picture"]).to eq("picture_value")
+        expect(hash["profile_url"]).to eq("https://dinopark.k8s.test.sso.allizom.org/p/username_value")
+      end
+    end
   end
 end

spec/components/mozilla_iam/authenticator_spec.rb
@@ -22,6 +22,7 @@ describe MozillaIAM::Authenticator do
       expect(result.email_valid).to      eq(true)
       expect(result.name).to             eq(user[:name])
       expect(result.extra_data[:uid]).to eq("ad|Mozilla-LDAP|#{user[:username]}")
+      expect(result.user_id).to          eq(result.extra_data[:uid])
     end
 
     it 'can authenticate and create a profile for an existing user' do
@@ -36,6 +37,7 @@ describe MozillaIAM::Authenticator do
       expect(result.email_valid).to      eq(true)
       expect(result.name).to             eq(user.name)
       expect(result.extra_data[:uid]).to eq(create_uid(user.username))
+      expect(result.user_id).to          eq(result.extra_data[:uid])
       expect(uid).to                     eq(create_uid(user.username))
       expect(last_refresh).to            be_within(5.seconds).of Time.now
     end
@@ -63,6 +65,7 @@ describe MozillaIAM::Authenticator do
       expect(result.email_valid).to      eq(true)
       expect(result.name).to             eq(user.name)
       expect(result.extra_data[:uid]).to eq(create_uid(user.username))
+      expect(result.user_id).to          eq(result.extra_data[:uid])
       expect(uid).to                     eq(create_uid(user.username))
       expect(last_refresh).to            be_within(5.seconds).of Time.now
     end
@@ -92,6 +95,7 @@ describe MozillaIAM::Authenticator do
       expect(result.email_valid).to      eq(true)
       expect(result.name).to             eq(user.name)
       expect(result.extra_data[:uid]).to eq(new_uid)
+      expect(result.user_id).to          eq(result.extra_data[:uid])
       expect(uid).to                     eq(new_uid)
       expect(last_refresh).to            be_within(5.seconds).of Time.now
     end
@@ -160,20 +164,40 @@ describe MozillaIAM::Authenticator do
   end
 
   context '#after_create_account' do
-    it 'can create profile for new user' do
-      user = Fabricate(:user_with_secondary_email)
-      auth = { extra_data: { uid: create_uid(user.username) }}
+    let(:user) { Fabricate(:user_with_secondary_email) }
+    let(:authenticator) { MozillaIAM::Authenticator.new('auth0', trusted: true) }
+    before do
       MozillaIAM::Profile.stubs(:refresh_methods).returns([])
+    end
 
-      authenticator = MozillaIAM::Authenticator.new('auth0', trusted: true)
+    it 'can create profile for new user' do
+      auth = { extra_data: { uid: create_uid(user.username) }}
       result = authenticator.after_create_account(user, auth)
 
       uid = user.custom_fields['mozilla_iam_uid']
       last_refresh = Time.parse(user.custom_fields['mozilla_iam_last_refresh'])
+      dinopark_enabled = user.custom_fields['mozilla_iam_dinopark_enabled']
 
       expect(result).to       be_within(5.seconds).of Time.now
       expect(last_refresh).to be_within(5.seconds).of Time.now
       expect(uid).to          eq(create_uid(user.username))
+      expect(dinopark_enabled).to be_nil
+    end
+
+    context "with dinopark_enabled" do
+      it 'can create profile for new user' do
+        auth = { dinopark_enabled: true, extra_data: { uid: create_uid(user.username) }}
+        result = authenticator.after_create_account(user, auth)
+
+        uid = user.custom_fields['mozilla_iam_uid']
+        last_refresh = Time.parse(user.custom_fields['mozilla_iam_last_refresh'])
+        dinopark_enabled = user.custom_fields['mozilla_iam_dinopark_enabled']
+
+        expect(result).to       be_within(5.seconds).of Time.now
+        expect(last_refresh).to be_within(5.seconds).of Time.now
+        expect(uid).to          eq(create_uid(user.username))
+        expect(dinopark_enabled).to eq("t")
+      end
     end
   end
 end

spec/requests/users_controller_spec.rb
@@ -4,6 +4,55 @@ describe UsersController do
   let!(:user) { sign_in(Fabricate(:user, admin: true)) }
   let(:profile) { MozillaIAM::Profile.new(user, "uid") }
 
+  describe '#create' do
+    before do
+      User.any_instance.stubs(:active?).returns(true)
+      UsersController.any_instance.stubs(:honeypot_value).returns(nil)
+      UsersController.any_instance.stubs(:challenge_value).returns(nil)
+    end
+
+    let(:create_params) do
+      {
+        name: "Jill Bloggs",
+        username: "jillbloggs",
+        password: "supersecret",
+        email: "jill@example.com"
+      }
+    end
+
+    context "without dinopark_enabled" do
+      it "creates user as normal" do
+        post "/u.json", params: create_params.merge({
+          dinopark_enabled: false
+        })
+        expect(response.status).to eq 200
+        expect(User.find(JSON.parse(response.body)["user_id"]).username).to eq "jillbloggs"
+        expect(session[:authentication]&.[](:dinopark_enabled)).to be_nil
+      end
+    end
+
+    context "with dinopark_enabled" do
+      it "creates user and sets dinopark_enabled flag in auth data" do
+        post "/u.json", params: create_params.merge({
+          dinopark_enabled: true
+        })
+        expect(response.status).to eq 200
+        expect(User.find(JSON.parse(response.body)["user_id"]).username).to eq "jillbloggs"
+        expect(session[:authentication]&.[](:dinopark_enabled)).to eq true
+      end
+
+      it "uses unique username if its taken" do
+        Fabricate(:user, username: "jillbloggs")
+        post "/u.json", params: create_params.merge({
+          dinopark_enabled: true
+        })
+        expect(response.status).to eq 200
+        expect(User.find(JSON.parse(response.body)["user_id"]).username).to eq "jillbloggs1"
+        expect(session[:authentication]&.[](:dinopark_enabled)).to eq true
+      end
+    end
+  end
+
   describe '#username' do
     let(:new_username) { user.username + "_1" }
 