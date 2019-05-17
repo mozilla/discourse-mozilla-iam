381e9a0639707860c80702a848946bac8085e895
Leo McArdle <lmcardle@mozilla.com>
2019-05-17 16:46:32 +0100

    UI: remove full name instructional text
    https://github.com/mozilla/discourse/issues/203

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: mozilla-iam
 # about: A plugin to integrate Discourse with Mozilla's Identity and Access Management (IAM) system
-# version: 1.2.0-alpha.24
+# version: 1.2.0-alpha.25
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-mozilla-iam
 
