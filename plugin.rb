1fd5d777a2ffc82d7a76caa8954c267bddd09e0e
Leo McArdle <lmcardle@mozilla.com>
2019-06-27 14:05:41 +0100

    update copy on dinopark sign up prompt

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: mozilla-iam
 # about: A plugin to integrate Discourse with Mozilla's Identity and Access Management (IAM) system
-# version: 1.2.0
+# version: 1.2.1
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-mozilla-iam
 
